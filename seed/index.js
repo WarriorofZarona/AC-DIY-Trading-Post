708abb69dc3695521b8b765f2696a7092e11393b
Andrew Bautista <andrewbautista85@gmail.com>
2020-06-10 10:44:30 -0400

    adjusted data to include model and documents properties, seeds recipes perfectly now

--- seed/index.js
+++ seed/index.js
@@ -1,18 +1,20 @@
 const seeder = require('mongoose-seed');
 const recipes = require('./recipes.json')
 const db = "mongodb://localhost/acdiydb";
-require("../models/Recipe.js")
 
-const data = recipes.map(recipe => {
-    return {
-        "name": recipe.name,
-        "image": recipe.image,
-        "category": recipe.category,
-        "materials": recipe.materials,
-        "source": recipe.source,
-        "notes": recipe.sourceNotes
-    }
-})
+const data = [{
+    "model": "Recipe",
+    "documents": recipes.map(recipe => {
+        return {
+            "name": recipe.name,
+            "image": recipe.image,
+            "category": recipe.category,
+            "materials": recipe.materials,
+            "source": recipe.source,
+            "notes": recipe.sourceNotes
+        }
+    })
+}]
 
 seeder.connect(db, () => {
     seeder.loadModels(["./models/Recipe.js"]);
