b3fe1b776c1db00acf6d6799ec2ed79102452455
Andrew Bautista <andrewbautista85@gmail.com>
2020-06-10 18:11:59 -0400

    installed bcryptjs, added it to the user model, added password as default value

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcryptjs = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
 
@@ -22,13 +23,27 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     //Password
-    password: Schema.Types.Mixed,
+    password: {
+        type: Schema.Types.Mixed,
+        default: "password"
+    },
     //Offerings
     offerings: Schema.Types.ObjectId,
     //Wishlist
     wishlist: Schema.Types.ObjectId
 
 });
+
+// Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+// Hooks are automatic methods that run during various phases of the User Model lifecycle
+// In this case, before a User is created, we will automatically hash their password
+User.addHook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+});
+
 
 const User = mongoose.model("User", userSchema);
 
